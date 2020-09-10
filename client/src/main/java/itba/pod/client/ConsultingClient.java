d39f2c8fd3e68395d6468b695616370949ced4f5
Micaela Banfi <mbanfi@itba.edu.ar>
2020-09-10 20:07:40 -0300

    comment tests

--- client/src/main/java/itba/pod/client/ConsultingClient.java
+++ client/src/main/java/itba/pod/client/ConsultingClient.java
@@ -1,6 +1,8 @@
 package itba.pod.client;
 
 
+import itba.pod.api.interfaces.ConsultingServiceInterface;
+import itba.pod.api.model.election.ElectionException;
 import itba.pod.api.model.election.Results;
 import itba.pod.api.model.election.Status;
 import itba.pod.api.model.vote.Candidate;
@@ -9,21 +11,55 @@ import itba.pod.api.model.vote.Table;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.security.Provider;
 import java.util.LinkedList;
 import java.util.List;
 
-public class ConsultingClient {
-    private Table table;
-    private State state;
+import static java.lang.System.exit;
 
-    public ConsultingClient(Table table, State state) {
-        this.table = table;
-        this.state = state;
+public class ConsultingClient {
+
+
+
+    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+        String serverAddress = System.getProperty("serverAddress");
+        String stateName = System.getProperty("state");
+        String pollingPlaceNumber = System.getProperty("id");
+        String fileName = System.getProperty("outPath");
+
+        if(stateName!=null && pollingPlaceNumber!=null){
+            System.out.println("Invalid Params (stateName or poolingPlace");
+            exit(-1);
+        }
+        try {
+            Results result;
+            final ConsultingServiceInterface service  = (ConsultingServiceInterface) Naming.lookup("//" + serverAddress + "/consulting-service");
+            if(stateName==null && pollingPlaceNumber==null){
+                result=service.getNationalResults();
+            }
+            else if(stateName!=null){
+                result=service.getStateResults(State.valueOf(stateName));
+            }
+            else {
+                result=service.getTableResults(new Table(Integer.parseInt(pollingPlaceNumber)));
+            }
+            CSVResults(result,fileName,stateName,pollingPlaceNumber);
+        }
+        catch (RemoteException | NotBoundException | MalformedURLException | ElectionException e){
+            System.out.println(e.getMessage());
+            exit(-1);
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public void CSVResults(Results results, String path) throws IOException, ClientException {
-        int provinceRounds=3;
+    public static void CSVResults(Results results, String path,String state,String table) throws ClientException {
+        int provinceRounds = 3;
         if (results.getStatus() == Status.NOT_INITIALIZED) {
             throw new ClientException("Election not initialize");
         }
@@ -37,28 +73,27 @@ public class ConsultingClient {
                     sb.append("Score;Party\n");
                     results.getWinners().get(0).forEach((k, v) -> sb.append(v.intValue()).append(";").append(k).append("\n"));
                     sb.append("Percentage;Party\n");
-                    results.getWinners().get(1).forEach((k, v) -> sb.append(String.format("%.2f", v*100)).append("%;").append(k).append("\n"));
+                    results.getWinners().get(1).forEach((k, v) -> sb.append(String.format("%.2f", v * 100)).append("%;").append(k).append("\n"));
                     sb.append("Winner\n");
                     sb.append(results.getWinners().get(1).entrySet().iterator().next().getKey()).append("\n");
                 } else if (table == null) {//provincial
-                    List<Candidate> aux=new LinkedList<>();
-                    for(int i=0;i<provinceRounds;i++){
-                        sb.append("Round ").append(i+1).append("\n");
+                    List<Candidate> aux = new LinkedList<>();
+                    for (int i = 0; i < provinceRounds; i++) {
+                        sb.append("Round ").append(i + 1).append("\n");
                         sb.append("Approval;Party\n");
                         results.getWinners().get(i).forEach((k, v) -> sb.append(String.format("%.2f", v)).append(";").append(k).append("\n"));
                         sb.append("Winners\n");
                         aux.add(results.getWinners().get(i).entrySet().iterator().next().getKey());
-                        for(Candidate c:aux){
+                        for (Candidate c : aux) {
                             sb.append(c);
-                            if(c!=aux.get(aux.size()-1))
+                            if (c != aux.get(aux.size() - 1))
                                 sb.append(",");
                         }
                         sb.append("\n");
                     }
-                }
-                else{
+                } else {
                     sb.append("Percentage;Party\n");
-                    results.getWinners().get(0).forEach((k, v) -> sb.append(String.format("%.2f", v*100)).append("%;").append(k).append("\n"));
+                    results.getWinners().get(0).forEach((k, v) -> sb.append(String.format("%.2f", v * 100)).append("%;").append(k).append("\n"));
                     sb.append("Winner\n");
                     sb.append(results.getWinners().get(0).entrySet().iterator().next().getKey()).append("\n");
 
